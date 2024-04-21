0abd3a3c6eb0f1e42fc16e5dd5dbe5c75ec4e6a3
bammcsapat <boglarka.soos@stud.uni-corvinus.hu>
2024-04-21 20:42:47 +0200

    ProductController tesztek

---
+++ KliensAlkalmazas/KliensAlkalmazas/Controllers/ProductController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace KliensAlkalmazas.Controllers
+{
+    public class ProductController
+    {
+        public Product ModifyPrice(Product product, decimal Price)
+        {
+
+            try
+            {
+                if (!ValidatePrice(Price.ToString()));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            product.Price = Price;
+            return product;
+
+        }
+
+        public Product ModifyDescription(Product product, string Desc)
+        {
+
+            try
+            {
+                if (!ValidatePrice(Desc.ToString()));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            product.Desc = Desc;
+            return product;
+
+        }
+
+        public bool ValidatePrice(string Price)
+        {
+            Regex regex = new Regex(@"^(0|[1-9]\d*)$");
+            return regex.IsMatch(Price);
+        }
+
+        public bool ValidateDesc(string Desc)
+        {
+            Regex regex = new Regex(@"^[A-Z].{48,}\.$");
+            return regex.IsMatch(Desc);
+        }
+    }
+}
---
+++ KliensAlkalmazas/UnitTest.Testt/ProductControllerTest.cs
@@ -0,0 +1,80 @@
+﻿using KliensAlkalmazas;
+using KliensAlkalmazas.Controllers;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.Testt
+{
+    public class ProductControllerTest
+    {
+        /*Product product1 = new Product();
+        Product product2 = new Product();
+
+        [Test,
+         TestCase(product1, 1000), 
+         TestCase(product2, 2000)
+         ]
+        public void TestModifyPrice(Product product, decimal newPrice)
+        {
+            //Arrange
+            var productcontroller = new ProductController();
+
+            // Act
+            var actualProduct = productcontroller.ModifyPrice(product, newPrice);
+
+            // Assert
+            ClassicAssert.AreEqual(newPrice, actualProduct.Price);
+        }*/
+
+        [Test,
+        TestCase("15000,0", false),
+        TestCase("-15000", false),
+        TestCase("015000", false),
+        TestCase("15000", true)
+         ]
+
+        public void TestValidatePrice(string Price, bool expectedResult) 
+        {
+            //Arrange
+            var productcontroller = new ProductController();
+
+            // Act
+            var actualResult = productcontroller.ValidatePrice(Price);
+
+            //Assert
+            ClassicAssert.AreEqual(expectedResult, actualResult);
+
+        }
+
+
+        [Test,
+        TestCase("", false),
+        TestCase("ez egy gyűrű", false),
+        TestCase("Ez egy gyűrű", false),
+        TestCase("Ez egy gyűrű.", false),
+        TestCase("ezüst színű elegáns karikagyűrű, apró, fényes kövekkel díszítve", false),
+        TestCase("ezüst színű elegáns karikagyűrű, apró, fényes kövekkel díszítve.", false),
+        TestCase("Ezüst színű elegáns karikagyűrű, apró, fényes kövekkel díszítve", false),
+        TestCase("Ezüst színű elegáns karikagyűrű, apró, fényes kövekkel díszítve.", true)
+         ]
+
+        public void TestValidateDesc(string Desc, bool expectedResult)
+        {
+            //Arrange
+            var productcontroller = new ProductController();
+
+            // Act
+            var actualResult = productcontroller.ValidateDesc(Desc);
+
+            //Assert
+            ClassicAssert.AreEqual(expectedResult, actualResult);
+
+        }
+    }
+}
