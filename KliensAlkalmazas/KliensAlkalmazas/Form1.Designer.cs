bd1cc46539943b0ac20b5b96688a9e30c061593b
Amrich Csongor <csongor.amrich@stud.uni-corvinus.hu>
2024-04-23 14:24:12 +0200

    Form1 maximize box false

--- KliensAlkalmazas/KliensAlkalmazas/Form1.Designer.cs
+++ KliensAlkalmazas/KliensAlkalmazas/Form1.Designer.cs
@@ -236,6 +236,7 @@
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Termékkezelő Kliensalkalmazás";
             this.panel1.ResumeLayout(false);
